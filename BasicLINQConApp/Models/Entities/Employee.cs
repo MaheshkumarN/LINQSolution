1c65ed4b27982af1603a3f9e1b544baf1494a205
Maheshkumar N <pn.maheshkumar@gmail.com>
2023-05-25 16:46:09 +0530

    Start

---
+++ BasicLINQConApp/Models/Entities/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace BasicLINQConApp.Models.Entities
+{
+  public class Employee
+  {
+    int _age;
+    string _empName;
+
+    public int Age
+    { 
+      get { return _age; }
+      set 
+      {
+        if (value < 1) throw new Exception("Age cannot be less than 1");
+        _age = value; 
+      }
+    }
+
+    public string EmpName
+    {
+      get { return _empName; }
+      set { _empName = value; }
+    }
+
+  }
+}
--- BasicLINQConApp/Program.cs
+++ BasicLINQConApp/Program.cs
@@ -1,5 +1,14 @@
-﻿#region Default
+﻿using static System.Console;
+using BasicLINQConApp.Models.Entities;
+
+#region Default
 //// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
 #endregion
+
+
+Employee emp = new Employee();
+emp.Age = 10;
+emp.EmpName = "Phantom";
+WriteLine($"{emp}");
